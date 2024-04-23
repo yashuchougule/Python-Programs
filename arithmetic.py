46c90d0c55aa24d1008fe75f6890c1654d710b86
DSi <yashchougule9@gmail.com>
2024-04-23 17:18:01 +0530

    Substraction Code

--- arithmetic.py
+++ arithmetic.py
@@ -3,7 +3,15 @@ def addition(no1, no2):
     return ans
 
 
+def substraction(no1, no2):
+    ans = no1 - no2
+    return ans
+
+
 a = 10
 b = 20
 ans = addition(a, b)
-print(f"The addition of {a} and {b} is:{ans}")
+print(f"The addition of {a} and {b} is: {ans}")
+
+ans = substraction(a, b)
+print(f"The Substraction of {a} and {b} is: {ans}")
